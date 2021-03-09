ac115ad836188cfb7c3001bb22bd97b3e37b59f1
Kotaina <kotaina217+git@gmail.com>
2021-03-09 11:50:03 +0300

    react init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./css/main.scss"
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="main-wrapper">
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
